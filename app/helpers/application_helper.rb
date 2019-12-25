12db62b32ff41193ba31ae7f7969b17219acceaa
dewamakoto <kzspreonerad86@yahoo.co.jp>
2019-12-25 05:49:19 +0000

    fix_chat_layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def facebook_url
+    	case Rails.env
+    when "development"
+      "https://www.facebook.com/dialog/oauth?client_id=2185436208425326&redirect_uri=https%3A%2F%2Flocalhost:3000%2Fusers%2Fauth%2Ffacebook%2Fcallback&scope=email"
+    when "production"
+      "https://www.facebook.com/dialog/oauth?client_id=2185436208425326&redirect_uri=https%3A%2F%2Fwww.2019gather.me%2Fusers%2Fauth%2Ffacebook%2Fcallback&scope=email"
+    end
+end
 end
